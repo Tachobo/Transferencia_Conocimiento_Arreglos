6e7d2ea9ba9e768a4341f17e8aa4ff72c086af8c
Tachobo <wilmerardila2001@gmail.com>
2025-11-10 23:35:52 -0500

    Cuarto Commit: lista tareas

---
+++ Tareas.js
@@ -0,0 +1,10 @@
+/*Se crea la lista de tareas totalmente vacia*/
+let tareas = [];
+/*Con push añadimos 3 tareas*/
+tareas.push("Programación");
+tareas.push("Ingles");
+tareas.push("Español");
+tareas.pop();
+
+console.log("Resultado Final: ", tareas);
+
