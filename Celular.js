753f22fe666da5cbf31e6611831d03fff3b09dd8
Tachobo <wilmerardila2001@gmail.com>
2025-11-10 23:29:54 -0500

    Tercer Commit: Celular y caracteristicas

---
+++ Celular.js
@@ -0,0 +1,12 @@
+/*Asignamos la variable celular, la cual contendra unas caracteristicas*/
+let celular = {
+    Marca: "Asus",
+    Modelo: "Max M1",
+    Año: "2019",
+    Estado: "Paso a mejor vida"
+}
+/*Imprimimos las caracteristicas individualmente*/
+console.log("Marca: ", celular.Marca);
+console.log("Modelo: ", celular.Modelo);
+console.log("Año: ", celular.Año);
+console.log("Estado: ", celular.Estado);
